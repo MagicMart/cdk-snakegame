fa1e2db8b141b35bf59e7f8ae9fac07f9b0ac8e5
Martin Tudor <martcan27@gmail.com>
2021-12-16 16:15:39 +0000

    add new resource path

--- lib/api_dynamo-stack.ts
+++ lib/api_dynamo-stack.ts
@@ -5,43 +5,51 @@ import {
   RemovalPolicy,
   Stack,
   StackProps,
-} from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { aws_s3 as s3 } from 'aws-cdk-lib';
+} from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+import { aws_s3 as s3 } from 'aws-cdk-lib'
 
 export class ApiDynamoStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
     const bucket = new s3.Bucket(this, 'ApiDynamoBucket', {
       autoDeleteObjects: true,
       removalPolicy: RemovalPolicy.DESTROY,
-    });
+    })
 
     const table = new dynamo.Table(this, 'ScoreboardTable', {
       partitionKey: { name: 'UserName', type: dynamo.AttributeType.STRING },
-      sortKey: { name: 'BestScore', type: dynamo.AttributeType.NUMBER },
       removalPolicy: RemovalPolicy.DESTROY,
-    });
+    })
 
     table.addGlobalSecondaryIndex({
       indexName: 'score-index',
       partitionKey: { name: 'Game', type: dynamo.AttributeType.STRING },
       sortKey: { name: 'BestScore', type: dynamo.AttributeType.NUMBER },
       projectionType: dynamo.ProjectionType.ALL,
-    });
+    })
     // defines an AWS Lambda resource
     const topScores = new lambda.Function(this, 'TopScoresHandler', {
       runtime: lambda.Runtime.NODEJS_14_X, // execution environment
       code: lambda.Code.fromAsset('lambda'), // code loaded from "lambda" directory
-      handler: 'top.handler', // file is "hello", function is "handler"
+      handler: 'topscores.handler', // file is "hello", function is "handler"
       environment: {
         TABLE_NAME: table.tableName,
       },
-    });
+    })
+    const updateScore = new lambda.Function(this, 'UpdateScoreHandler', {
+      runtime: lambda.Runtime.NODEJS_14_X, // execution environment
+      code: lambda.Code.fromAsset('lambda'), // code loaded from "lambda" directory
+      handler: 'updateScore.handler', // file is "hello", function is "handler"
+      environment: {
+        TABLE_NAME: table.tableName,
+      },
+    })
 
     // grant read/write permissions to the table
-    table.grantReadData(topScores);
+    table.grantReadData(topScores)
+    table.grantReadWriteData(updateScore)
 
     // defines an API Gateway REST API resource backed by our "hello" function.
     // new apigw.LambdaRestApi(this, 'Endpoint', {
@@ -49,9 +57,12 @@ export class ApiDynamoStack extends Stack {
     // });
     const api = new apigw.RestApi(this, 'Endpoint2', {
       restApiName: 'ApiDynamo',
-    });
+    })
     api.root
-      .resourceForPath('/scoreboard/top')
-      .addMethod('GET', new apigw.LambdaIntegration(topScores));
+      .resourceForPath('/scoreboard/topscores')
+      .addMethod('GET', new apigw.LambdaIntegration(topScores))
+    api.root
+      .resourceForPath('/scoreboard/{user}/updatescore/{score}')
+      .addMethod('PUT', new apigw.LambdaIntegration(updateScore))
   }
 }
