dbd2c1017824502df3d7b65de4edf247df252314
Martin Tudor <martcan27@gmail.com>
2021-12-17 17:22:57 +0000

    refactor

--- lambda/updateScore.ts
+++ lambda/updateScore.ts
@@ -2,8 +2,35 @@ import { ScoreRepository } from './ScoreRepository'
 
 const ScoreClient = new ScoreRepository()
 export const handler = async function (event: any) {
-  const user: string = event.pathParameters.user
-  const score = Number(event.pathParameters.score)
+  if (!event.body)
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: true, message: 'Invalid request' }),
+    }
+
+  const body = JSON.parse(event.body)
+  const score = Number(body.score)
+  if (!Number.isInteger(score)) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        error: true,
+        message: 'score must be an integer',
+      }),
+    }
+  }
+
+  const user = body.user
+
+  if (!user) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        error: true,
+        message: 'Missing user',
+      }),
+    }
+  }
   let result
   try {
     result = await ScoreClient.updateScore(user, score)
